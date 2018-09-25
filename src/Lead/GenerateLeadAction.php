3c7482769f07efcdd17e10526b810e1c083539c3
Jan Mikeš <j.mikes@me.com>
2018-09-25 15:02:53 +0200

    Added config

--- src/Lead/GenerateLeadAction.php
+++ src/Lead/GenerateLeadAction.php
@@ -5,9 +5,23 @@ namespace Entrydo\Pipedrive\Lead;
 use BrandEmbassy\Slim\ActionHandler;
 use BrandEmbassy\Slim\Request\RequestInterface;
 use BrandEmbassy\Slim\Response\ResponseInterface;
+use Devio\Pipedrive\Pipedrive;
+use Devio\Pipedrive\Resources\Persons;
 
 class GenerateLeadAction implements ActionHandler
 {
+	/**
+	 * @var Pipedrive
+	 */
+	private $pipedrive;
+
+
+	public function __construct(Pipedrive $pipedrive)
+	{
+		$this->pipedrive = $pipedrive;
+	}
+
+
 	/**
 	 * @param mixed[] $arguments
 	 *
@@ -26,6 +40,18 @@ class GenerateLeadAction implements ActionHandler
 	 */
 	public function __invoke(RequestInterface $request, ResponseInterface $response, array $arguments = []): ResponseInterface
 	{
+		/** @var $personsResource Persons */
+		$personsResource = $this->pipedrive->make('persons');
+
+		// 1. Hledat společnost
+		// je/neni vytvoří/najde
+
+		// 2. hledat osobu
+		// je/neni vytvoří/najde
+
+		// 3. vytvořit deal
+
+
 		return $response->withJson(['status' => 'ok'], 201);
 	}
 }
